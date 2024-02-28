6652b7adf92e312529b1a2fa1d52d1e3c61cc1e4
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-02-28 18:45:33 +0100

    main task ended

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default { components:{
+  AppTitle,
+}}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="vitellino">
+    <h1>
+      manzotin
+    </h1>
+    <h2>vit-hello</h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppTitle></AppTitle>
+ 
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.vitellino{
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  
+  text-decoration: underline;
+  text-transform: uppercase;
+  background-color: darkblue;
+  border-radius: 20px;
+  text-align: center;
+  padding: 40px;
+  color: rgb(31, 226, 70);
+  font-family:   sans-serif ;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+ 
+
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,26 @@
+<script>
+export default{
+    name: "AppTitle"
+}
+</script>
+
+
+
+<template>
+    <p> La mia prima app con vite!</p>
+
+</template>
+
+<style>
+p{
+    font-size: larger;
+    font-weight: bolder;
+    color: red;
+    border: 1px solid black;
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    margin-left: 80px;
+ 
+}
+</style>
